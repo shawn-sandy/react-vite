da5cb68fd02d5922a3bc850d5da71891e4adbe30
Shawn Sandy <creative@shawnsandy.design>
2022-06-19 20:43:50 -0400

    feat: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
+/** @format */
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    dts(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/component/Link.tsx'),
+      name: 'MyLib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
+  },
 })
