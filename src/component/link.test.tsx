6875320db8b4bb31a589f61b51d15f26c826df66
Shawn Sandy <ssandy@indeed.com>
2022-06-21 17:48:03 -0400

    test: :sparkles: assert label is defined in document

--- src/component/link.test.tsx
+++ src/component/link.test.tsx
@@ -4,7 +4,7 @@ import { describe, expect, test, it  } from 'vitest';
 import { render, screen  } from '@testing-library/react';
 import Link from"./Link"
 
-describe("Test link", () => {
+describe("<Link>", () => {
   test("component renders as expected to", () => {
     const container = render(<Link />)
     expect(screen.getByText('Default Link')).toBeDefined()
@@ -15,11 +15,12 @@ describe("Test link", () => {
   it("should accept props", () => {
 
     const link = "https://google.com/"
-    const label = " Search on Google"
+    const label = "Search on Google"
+
     const linkContainer = render(<Link page={link}>{label}</Link>)
     const linkElement = screen.getByRole('link');
     screen.debug(linkElement)
-
+    expect(screen.getByText(label)).toBeDefined()
     expect(linkContainer).toMatchSnapshot()
     screen.debug()
 
