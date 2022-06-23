fd2e97503c5e1abe725ea6c43471ae4d1fdb9536
Shawn Sandy <ssandy@indeed.com>
2022-06-23 16:35:14 -0400

    build/dev updates

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTest.ts',
+    setupFiles: './src/setupTest.ts',
     coverage: {
       reporter: ['text', 'html'],
       exclude: [
