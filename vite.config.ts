6f95c24511c5c5dd59617eaa878f4a9de73b7ea2
shawn <shawnsandy04@gmail.com>
2022-07-08 18:13:49 -0400

    chore: 👷 remove package code from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,7 @@
-/** @format */
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  if (mode !== 'package') {
-    return {
-      plugins: [react()],
-    }
-
-  } else {
-    return {
-      plugins: [react(), dts()],
-      esbuild: {
-        logOverride: { 'this-is-undefined-in-esm': 'silent' }
-      },
-      build: {
-        outDir: resolve(__dirname, '../lib'),
-        sourcemap: true,
-        lib: {
-          entry: resolve(__dirname, 'src/component/index.ts'),
-          name: 'Link',
-          formats: ['es', 'umd'],
-          fileName: (format) => `index.${format}.js`,
-        },
-        rollupOptions: {
-          external: ['react', 'react-dom', 'styled-components'],
-          output: {
-            globals: {
-              react: 'React',
-              'react-dom': 'ReactDOM',
-            },
-          },
-        },
-      },
-    }
-  }
+export default defineConfig({
+  plugins: [react()]
 })
