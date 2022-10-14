fe2bb8c29e06cf10fbfda25ed07e3f407773be57
shawn <shawnsandy04@gmail.com>
2022-10-14 12:01:04 -0400

    feat: code updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-import {Button} from '@fpkit/fp-button'
-import {Badge, FP} from '@fpkit/fp'
+// import {Button} from '@fpkit/fp-button'
+import {Badge, FP, Dropdown, Card, Button, Kit, Box } from '@fpkit/fp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -44,6 +42,27 @@ function App() {
         <p>
         <FP as='h3'>FP KIt <Badge elm="span" role="note">Beta</Badge></FP>
         </p>
+
+        <p>
+        <Kit as='h3'>FP KIt <Badge elm="span" role="note">Kit Beta</Badge></Kit>
+        </p>
+
+        <p>
+        <Box as='h3'>FP KIt <Badge elm="span" role="note">Beta</Badge></Box>
+        </p>
+        <Card elm="aside">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam iusto suscipit ea vitae, accusamus ipsum. Nobis nihil deserunt enim debitis laboriosam suscipit, eius impedit dicta ex animi. Quos, voluptatum assumenda?</p>
+        </Card>
+        <Dropdown summary='Dropdown' title='' styl={{backgroundColor: "black", textAlign: "left"}}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum unde ex eos cum magnam. Et veniam laudantium commodi similique, temporibus beatae porro. Eum, recusandae. Minus voluptatum quisquam consequatur voluptate est! <Badge role='note'>Beta</Badge>
+          </p>
+          </Dropdown>
+          <p>
+          <Card>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quibusdam quo dignissimos, error incidunt tempore facere illo dolore dicta eius dolorum nesciunt, facilis obcaecati. Placeat sunt dolor eveniet explicabo minima.
+          </Card>
+          </p> 
       </header>
     </div>
   )
