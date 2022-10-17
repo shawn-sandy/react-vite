15772d5ea91a5c0e613b7a4dd82154a7211f8aad
shawn <shawnsandy04@gmail.com>
2022-10-17 07:37:42 -0400

    ci: add sample config files

--- src/component/demo-component.tsx
+++ src/component/demo-component.tsx
@@ -1,17 +1,19 @@
-import * as React from 'react';
+import * as React from "react";
 
 export interface DemoComponentProps {
-    title: string;
-    children: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
 }
 
-export const DemoComponent: React.FC<DemoComponentProps> = ({ title, children, ...props }) => (
-    <>
+export const DemoComponent: React.FC<DemoComponentProps> = ({
+  title,
+  children,
+  ...props
+}) => (
+  <>
     <h1>{title}</h1>
-    <div {...props}>
-        {children}
-    </div>
-    </>
-)
+    <div {...props}>{children}</div>
+  </>
+);
 
 export default DemoComponent;
